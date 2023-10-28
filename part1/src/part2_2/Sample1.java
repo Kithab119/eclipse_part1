a78feb4ff0ed635e1568179dc638c1b9419c2dba
Kithab119 <yuui.furugaki@gmail.com>
2023-10-28 23:11:40 +0900

    LinkedHashSet追加順に保存

--- part1/src/part2_2/Sample1.java
+++ part1/src/part2_2/Sample1.java
@@ -1,12 +1,12 @@
 package part2_2;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class Sample1 {
 
 	public static void main(String[] args) {
 		
-		var set = new HashSet<Meibo>();
+		var set = new LinkedHashSet<Meibo>();
 		set.add(new Meibo(100, "田中宏"));
 		set.add(new Meibo(200, "木村正一"));
 		set.add(new Meibo(100, "田中宏"));
