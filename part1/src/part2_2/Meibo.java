4f0fc8f996eaf8b91d70fda5352748cbedbb51ae
Kithab119 <yuui.furugaki@gmail.com>
2023-10-28 23:21:28 +0900

    Comparableインタフェースで引数の大小比較

--- part1/src/part2_2/Meibo.java
+++ part1/src/part2_2/Meibo.java
@@ -1,3 +1,8 @@
 package part2_2;
 
-public record Meibo(int number, String name) {}
+public record Meibo(int number, String name) implements Comparable<Meibo>{
+
+	@Override
+	public int compareTo(Meibo o) {
+		return Integer.compare(number, o.number);
+	}}
