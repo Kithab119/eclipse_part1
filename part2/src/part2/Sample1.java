f16634c8d8bff9e5caf863f094526b3d2893e751
Kithab119 <yuui.furugaki@gmail.com>
2023-10-31 23:05:20 +0900

    stream.distinct,sorted

---
+++ part2/src/part2/Sample1.java
@@ -0,0 +1,17 @@
+package part2;
+
+import java.util.List;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		
+		var list = List.of("apple", "peach", "grape", "apple", "banana", "peach");
+		
+		list.stream()
+			.distinct()
+			.sorted()
+			.forEach(System.out::println);
+	}
+
+}
