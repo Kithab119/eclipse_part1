d01515cc31270737cc87d8c16da4131d48fd4e07
Kithab119 <yuui.furugaki@gmail.com>
2023-11-02 23:23:06 +0900

    可変list、可変Set

--- part2/src/part2/Sample8.java
+++ part2/src/part2/Sample8.java
@@ -1,9 +1,18 @@
 package part2;
 
+import java.util.stream.Collectors;
+
 public class Sample8 {
 
 	public static void main(String[] args) {
-
+		
+		var list = Book.getBookList();
+		var myList = list.stream().collect(Collectors.toList());
+		var mySet = list.stream().collect(Collectors.toSet());
+		
+		//list.stream().forEach(System.out::println);
+		//myList.stream().forEach(System.out::println);
+		mySet.stream().forEach(System.out::println);
 	}
 
 }
