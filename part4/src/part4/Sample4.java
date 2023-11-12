056e65b644692aa3281ede69a01203833f5b077c
Kithab119 <yuui.furugaki@gmail.com>
2023-11-12 22:06:20 +0900

    列挙定数のソート

---
+++ part4/src/part4/Sample4.java
@@ -0,0 +1,16 @@
+package part4;
+
+import java.util.Arrays;
+
+public class Sample4 {
+
+	public static void main(String[] args) {
+
+		Size[] ss = {Size.MEDIUM, Size.LARGE, Size.SMALL};
+		Arrays.sort(ss);
+		for(Size  s : ss) {
+			System.out.println(s.name() + ":" + s.ordinal());
+		}
+		
+	}
+}
