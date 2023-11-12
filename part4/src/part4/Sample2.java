082412755b09040118af91877dee761ec398e904
Kithab119 <yuui.furugaki@gmail.com>
2023-11-12 21:51:39 +0900

    列挙型‥名前と序数

---
+++ part4/src/part4/Sample2.java
@@ -0,0 +1,11 @@
+package part4;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+
+		System.out.println(Size.SMALL.name());
+		System.out.println(Size.SMALL.ordinal());
+	}
+
+}
