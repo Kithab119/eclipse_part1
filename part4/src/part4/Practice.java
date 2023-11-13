c6ecb84183c8443370de6b50fa519027407f7718
Kithab119 <yuui.furugaki@gmail.com>
2023-11-13 23:30:04 +0900

    列挙型練習完了

---
+++ part4/src/part4/Practice.java
@@ -0,0 +1,11 @@
+package part4;
+
+public class Practice {
+
+	public static void main(String[] args) {
+
+		var book = new Book(Genre.CULTURE, "日本の祭り", "斎藤孝雄");
+		System.out.println(book);
+	}
+
+}
