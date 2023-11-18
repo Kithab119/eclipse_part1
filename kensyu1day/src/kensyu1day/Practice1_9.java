f4c05ae67b7889c6d07103da96866f5671b03f26
Kithab119 <yuui.furugaki@gmail.com>
2023-11-18 15:25:55 +0900

    研修会１日目　北ソフト工房４

---
+++ kensyu1day/src/kensyu1day/Practice1_9.java
@@ -0,0 +1,14 @@
+package kensyu1day;
+
+public class Practice1_9 {
+
+	public static void main(String[] args) {
+
+		int x = 2;
+		int a = x * 2;
+		int b = x * 3;
+		int c = x * 4;
+		System.out.println(a + "," + b + "," + c);
+	}
+
+}
