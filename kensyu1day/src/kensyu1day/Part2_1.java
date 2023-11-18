6a7b4631c09502a19772a9a7ba998ae2e6443394
Kithab119 <yuui.furugaki@gmail.com>
2023-11-18 14:20:11 +0900

    研修１日目　練習３

---
+++ kensyu1day/src/kensyu1day/Part2_1.java
@@ -0,0 +1,14 @@
+package kensyu1day;
+
+public class Part2_1 {
+
+	public static void main(String[] args) {
+
+		int x = 5;
+		int y = 10;
+		int a = x + y;
+		String ans = "x+yは" + a;
+		System.out.println(ans);
+	}
+
+}
