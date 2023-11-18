40fc427d6715770e7a11d8ae8d8e9385a82c61d6
Kithab119 <yuui.furugaki@gmail.com>
2023-11-18 15:33:20 +0900

    研修会１日目　北ソフト工房５

---
+++ kensyu1day/src/kensyu1day/Practice1_10.java
@@ -0,0 +1,14 @@
+package kensyu1day;
+
+public class Practice1_10 {
+
+	public static void main(String[] args) {
+
+		int x = 2;
+		int a = (int) Math.pow(x, 2);
+		int b = (int) Math.pow(x, 3);
+		int c = (int) Math.pow(x, 4);
+		System.out.println(a + "," + b + "," + c);
+	}
+
+}
