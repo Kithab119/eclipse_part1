3a160574aba320362fa92be5f160495579c5844b
Kithab119 <yuui.furugaki@gmail.com>
2023-11-18 14:15:36 +0900

    研修１日目　練習２

---
+++ kensyu1day/src/kensyu1day/Part1_3.java
@@ -0,0 +1,13 @@
+package kensyu1day;
+
+public class Part1_3 {
+
+	public static void main(String[] args) {
+
+		boolean b = true;
+		char c = '駆';
+		double d = 3.14;
+		long l = 314159265853979L;
+	}
+
+}
