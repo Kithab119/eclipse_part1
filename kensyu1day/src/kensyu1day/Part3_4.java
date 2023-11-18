f590a4b07cdf6d33e37f24d1196d698ec8770a11
Kithab119 <yuui.furugaki@gmail.com>
2023-11-18 17:32:50 +0900

    研修会１日目　練習７

---
+++ kensyu1day/src/kensyu1day/Part3_4.java
@@ -0,0 +1,18 @@
+package kensyu1day;
+
+public class Part3_4 {
+
+	public static void main(String[] args) {
+
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("映画を見ます");
+			System.out.println("寝ます");
+		}
+		//ifのあとは{}で閉じる
+	}
+
+}
