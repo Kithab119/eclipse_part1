352cc784cd354c1ffc7453de05f3a1f103e63072
Kithab119 <yuui.furugaki@gmail.com>
2023-11-18 17:19:55 +0900

    研修会１日目　練習５

---
+++ kensyu1day/src/kensyu1day/Part3_1.java
@@ -0,0 +1,21 @@
+package kensyu1day;
+
+public class Part3_1 {
+
+	public static void main(String[] args) {
+
+		int weight = 60;
+		if (weight == 60);
+		
+		int age1 = 3;
+		int age2 = 4;
+		if ((age1 * 2 + age2 * 2) > 60);
+		
+		int age = 5;
+		if (age % 2 != 0);
+		
+		String name = "田中";
+		if (name.equals("湊"));
+	}
+
+}
