0726c1fc071c721d3e10a271da85229fbaa388a7
Kithab119 <yuui.furugaki@gmail.com>
2023-11-20 21:43:20 +0900

    空白を表すメタ文字

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,7 +4,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("\\d+", "2301", "apple_#");
-		Reg.test("\\D+", "2301", "apple_#");
+		Reg.test("ap\\s+ple", "ap ple", "ap##ple");
+		Reg.test("ap\\S+ple", "ap ple", "ap##ple");
 	}
 }
