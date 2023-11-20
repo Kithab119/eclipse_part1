242f9b3cb50af0dbb85be8e6a3a0d2db7151de79
Kithab119 <yuui.furugaki@gmail.com>
2023-11-20 21:49:06 +0900

    英語を表すメタ文字

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,7 +4,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("ap\\s+ple", "ap ple", "ap##ple");
-		Reg.test("ap\\S+ple", "ap ple", "ap##ple");
+		Reg.test("\\w+", "apple_12", "#$%&");
+		Reg.test("\\W+", "apple_12", "#$%&");
 	}
 }
