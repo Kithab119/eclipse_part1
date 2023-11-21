68df950aa1203eb84cc402ee66bb8877c30d11c8
Kithab119 <yuui.furugaki@gmail.com>
2023-11-21 23:15:15 +0900

    正規表現のグループ化

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,6 +4,6 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("[TJ]im|[Tt]om|[Ss]am", "Jim", "Kim", "tom", "sam");
+		Reg.test("(1[012]|[1-9])(am|pm)", "10am", "8pm", "13pm");
 	}
 }
