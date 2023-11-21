542ee9b10ba1b531f4734558b7884e08739ab698
Kithab119 <yuui.furugaki@gmail.com>
2023-11-21 23:08:06 +0900

    どちらかの正規表現OR

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,8 +4,6 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("\\d{3}", "123", "1234", "12345", "123456");
-		Reg.test("\\d{3,}", "123", "1234", "12345", "123456");
-		Reg.test("\\d{3,5}", "123", "1234", "12345", "123456");
+		Reg.test("[TJ]im|[Tt]om|[Ss]am", "Jim", "Kim", "tom", "sam");
 	}
 }
