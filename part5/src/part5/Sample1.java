5956a5e78a31e3c3f58a5277f456b9478d06d146
Kithab119 <yuui.furugaki@gmail.com>
2023-11-21 23:20:42 +0900

    大文字と小文字を区別する

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,6 +4,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("(1[012]|[1-9])(am|pm)", "10am", "8pm", "13pm");
+		Reg.test("(?i)(tim|tom|sam)", "Tim", "Kim", "Tom", "Sam");
+		Reg.test("(?-i)(tim|tom|sam)", "Tim", "Kim", "Tom", "Sam");
 	}
 }
