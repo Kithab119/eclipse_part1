d19e91ce826d63bc6d81831d2e1eba1ddd49321a
Kithab119 <yuui.furugaki@gmail.com>
2023-11-22 23:39:56 +0900

    指定したパターンを含む場合

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,7 +4,6 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("(?i)(tim|tom|sam)", "Tim", "Kim", "Tom", "Sam");
-		Reg.test("(?-i)(tim|tom|sam)", "Tim", "Kim", "Tom", "Sam");
+		Reg.test("(?=.*tom).*", "xtom130", "tom", "tom.13$");
 	}
 }
