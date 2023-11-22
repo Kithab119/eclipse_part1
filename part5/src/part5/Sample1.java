54c8df694736ac9ae2bf19f0f4f0d3a8f8b41fa3
Kithab119 <yuui.furugaki@gmail.com>
2023-11-22 23:57:24 +0900

    含まない場合

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,6 +4,6 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("(?=.*tom)(?=.*[@#$%]).*", "xtom130", "tom", "tom.13$");
+		Reg.test("(?=.*tom)(?!.*[0-9]).*", "xtom130", "tom", "tom.13$");
 	}
 }
