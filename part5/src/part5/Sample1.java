38352ca988efa9bc66abe453f42c94e826724d13
Kithab119 <yuui.furugaki@gmail.com>
2023-11-22 23:49:41 +0900

    文字列パターンを含む場合

--- part5/src/part5/Sample1.java
+++ part5/src/part5/Sample1.java
@@ -4,6 +4,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		Reg.test("(?=.*tom).*", "xtom130", "tom", "tom.13$");
+		Reg.test("(?=.*tom).{6,10}", "xtom130", "tom", "tom.13$");
+		Reg.test("(?=.*tom)\\w+", "xtom130", "tom", "tom.13$");
 	}
 }
